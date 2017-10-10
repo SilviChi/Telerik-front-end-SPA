b3d9718383dfe69a6411733931be24a55262893c
Silvia Chilova-Slavova <silvia.chilova@gmail.com>
2017-10-10 13:56:05 +0300

    Location view

---
+++ src/assets/css/menus-controller.js
@@ -0,0 +1,25 @@
+import MenusDAO from 'menus-dao';
+import Templates from 'templates';
+
+class MenusController {
+	constructor() {
+		this.dao = new MenusDAO();
+	}
+
+	chefsRecommendations() {
+		this.dao.getAll()
+		.then((items) => {
+			items = items.filter((item) => {
+				return item.recommended;
+			});
+			new Templates('recommendations')
+				.show('.section1', {
+					items: [items[0], items[1], items[2]],
+				});
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
+}
+
+export default MenusController;
